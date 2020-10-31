9083ef9a2b32feda9916fca6b9102b256fbde7da
mariyafrantsava <mariya.frantsava7@gmail.com>
2020-10-31 21:52:00 +0300

    feat: finish dream-team of task basic-js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    let str = '';
+    if (members !== null && Array.isArray(members)) {
+        for (let i = 0; i < members.length; i++) {
+            if (typeof members[i] === 'string') {
+                str += members[i].trim().substring(0, 1).toUpperCase();
+            }
+        }
+        return str.split('').sort().join('');
+    } else {
+        return false;
+    }
 };
