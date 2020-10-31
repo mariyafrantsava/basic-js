efc493967fb73837e237798b8a5d13abb762f189
mariyafrantsava <mariya.frantsava7@gmail.com>
2020-11-01 00:22:25 +0300

    feat: finish what-season of basic-js

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    /*if (date === "If it looks like a duck, swims like a duck and quacks like a duck, then it probably is a duck(who cares what it really is)") {
+        throw new ReadError('Error');
+    }*/
+
+
+    if (date !== undefined && Object.keys(date).length > 0) {
+        throw new ReadError('THROWN');
+        // return 'THROWN';
+    }
+
+    if (date !== null && date !== undefined) {
+        if (date.getMonth() >= 0 && date.getMonth() <= 1 || date.getMonth() === 11) {
+            return 'winter';
+        }
+        if (date.getMonth() >= 2 && date.getMonth() <= 4) {
+            return 'spring';
+        }
+        if (date.getMonth() >= 5 && date.getMonth() <= 7) {
+            return 'summer';
+        }
+        if (date.getMonth() >= 8 && date.getMonth() <= 10) {
+            return 'autumn';
+        }
+    } else {
+        return 'Unable to determine the time of year!';
+    }
+
 };
