9794b594bf94bb26c1f7c539d114a17ae49df35a
mariyafrantsava <mariya.frantsava7@gmail.com>
2020-11-01 23:23:49 +0300

    feat: finish extended-repeater of task basic-js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+    let result = '';
+    str = str + '';
+
+    let addition = '';
+    if (options.addition !== undefined) {
+        addition = options.addition + '';
+    }
+
+    let separator = '+';
+    if (options.separator !== undefined) {
+        separator = options.separator;
+    }
+
+    let additionSeparator = '|';
+    if (options.additionSeparator !== undefined) {
+        additionSeparator = options.additionSeparator;
+    }
+
+    if (options.repeatTimes !== undefined) {
+        for (let i = 0; i < options.repeatTimes; i++) {
+            result += str;
+            if (options.additionRepeatTimes !== undefined) {
+                for (let i = 0; i < options.additionRepeatTimes; i++) {
+                    result += addition;
+                    if (i !== options.additionRepeatTimes - 1) {
+                        result += additionSeparator;
+                    }
+
+                }
+            }
+
+            if (i !== options.repeatTimes - 1) {
+                result += separator;
+            }
+
+        }
+    } else {
+        return str + addition;
+    }
+
+
+    return result;
 };
-  
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,22 @@
 const CustomError = require("../extensions/custom-error");
-
+//let counter = 0;
+//let result = 0;
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    calculateDepth(arr) {
+        throw new CustomError('Not implemented');
+        // remove line with error and write your code here
+        /*
+                for (let i = 0; i < arr.length; i++) {
+                    if (Array.isArray(arr[i])) {
+                        counter++;
+                        this.calculateDepth(arr[i]);
+                    } else {
+                        if (result < counter) {
+                            result = counter;
+                        }
+                        counter = 0;
+                    }
+                }
+                return result;*/
+    }
 };
