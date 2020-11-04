e06f84aada74fdc13129f3d6ac7a97ad2454e64b
mariyafrantsava <mariya.frantsava7@gmail.com>
2020-11-04 19:04:34 +0300

    feat: finish simple-chain of task basic-js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+    chain: [],
+
     getLength() {
-        throw new CustomError('Not implemented');
-        // remove line with error and write your code here
+        return this.chain.length();
     },
     addLink(value) {
-        throw new CustomError('Not implemented');
-        // remove line with error and write your code here
+        this.chain.push(`( ${value} )`);
+        return this;
     },
+
     removeLink(position) {
-        throw new CustomError('Not implemented');
-        // remove line with error and write your code here
+        if (typeof(position) == 'number' && position > 0 && Number.isInteger(position) == true && position <= this.chain.length) {
+            this.chain.splice(position - 1, 1);
+            return this;
+
+        }
+        this.chain = [];
+        throw new Error();
+
+
     },
+
     reverseChain() {
-        throw new CustomError('Not implemented');
-        // remove line with error and write your code here
+        this.chain.reverse();
+        return this;
     },
     finishChain() {
-        throw new CustomError('Not implemented');
-        // remove line with error and write your code here
+        let res = this.chain.join('~~');
+        this.chain = [];
+        return res;
     }
 };
 
